73cc4be45c77469406c8802e778e378f26d1a29c
André Machado <andrels.machado82@gmail.com>
2019-10-13 19:55:44 -0300

    Criando e estilizando página de Main

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -7,12 +7,33 @@ import {
   TextInput,
   TouchableOpacity,
 } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
 export default function Login({navigation}) {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', {user});
+      }
+    });
+  }, [navigation]);
+
   async function handleLogin() {
-    navigation.navigate('Main');
+    const response = await api.post('/devs', {
+      username: user,
+    });
+
+    const {_id} = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+
+    navigation.navigate('Main', {_id});
   }
 
   return (
@@ -24,6 +45,8 @@ export default function Login({navigation}) {
         placeholder="Digite seu usuário do GitHub"
         placeholderTextColor="#999"
         style={styles.input}
+        value={user}
+        onChangeText={setUser}
       />
 
       <TouchableOpacity onPress={handleLogin} style={styles.button}>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,115 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, SafeAreaView, Image, StyleSheet} from 'react-native';
 
-// import { Container } from './styles';
+import logo from '../assets/logo.png';
+import dislike from '../assets/dislike.png';
+import like from '../assets/like.png';
+
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 export default function Main() {
-  return <View />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      <View style={styles.cardsContainer}>
+        <View style={styles.card}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: 'https://avatars2.githubusercontent.com/u/49668949?v=4',
+            }}
+          />
+          <View style={styles.footer}>
+            <Text style={styles.name}>André Machado</Text>
+            <Text style={styles.bio} numberOfLines={3}>
+              Estudante de Sistemas de Internet
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Image source={dislike} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image source={like} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  logo: {
+    marginTop: 20,
+  },
+
+  cardsContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    maxHeight: 500,
+  },
+
+  card: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    margin: 30,
+    overflow: 'hidden',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+
+  avatar: {
+    flex: 1,
+    height: 300,
+  },
+
+  footer: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+
+  bio: {
+    fontSize: 14,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 18,
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginBottom: 30,
+  },
+
+  button: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    elevation: 2,
+  },
+});
