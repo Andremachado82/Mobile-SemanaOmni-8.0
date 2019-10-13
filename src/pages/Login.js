41cc6f37feb78200c9e3230089846eb7507d35f6
André Machado <andrels.machado82@gmail.com>
2019-10-13 18:33:05 -0300

    Criando e estilizando página de Login

---
+++ src/pages/Login.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import logo from '../assets/logo.png';
+
+export default function Login({navigation}) {
+  async function handleLogin() {
+    navigation.navigate('Main');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} />
+
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Digite seu usuário do GitHub"
+        placeholderTextColor="#999"
+        style={styles.input}
+      />
+
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#Df4723',
+    borderRadius: 4,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
